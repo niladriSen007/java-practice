ba0112d3b2c1437a8f8e3b61382b4d30651b255c
Niladri <niladrisengit12@gmail.com>
2025-11-13 16:35:25 +0530

    Add HashmapDemo implementation and update StreamPlaylist with additional character processing methods; modify CodeStream configuration

---
+++ src/coreImplementation/HashmapDemo.java
@@ -0,0 +1,89 @@
+package coreImplementation;
+
+import java.util.LinkedList;
+
+public class HashmapDemo {
+
+    static int THRESHOLD_OF_K = 2;
+
+    static class HashMap<K, V> {
+        private int n;
+        private int N;
+        private LinkedList<Node> buckets[];
+
+        public HashMap() {
+            this.N = 4;
+            this.buckets = new LinkedList[4];
+            for (int i = 0; i < 4; i++) {
+                this.buckets[i] = new LinkedList<Node>();
+            }
+        }
+
+        private int hashFunction(K key) {
+            int bucketIndex = key.hashCode();
+            return Math.abs(bucketIndex) % N;
+        }
+
+        private int searchIndexInLL(int bucketIndex, K key) {
+            LinkedList<Node> bucket = buckets[bucketIndex];
+            for (int i = 0; i < bucket.size(); i++) {
+                if (bucket.get(i).key.equals(key)) return i;
+            }
+            return -1;
+        }
+
+        private void rehashing() {
+            LinkedList<Node> oldBucket[] = buckets;
+            buckets = new LinkedList[N * 2];
+
+            //adding blank LL to the new bucket
+            for (int i = 0; i < buckets.length; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+
+            for (int i = 0; i < oldBucket.length; i++) {
+                LinkedList<Node> llBucket = oldBucket[i];
+                for (int j = 0; i < llBucket.size(); j++) {
+                    Node node = llBucket.get(i);
+                    put(node.key, node.value);
+                }
+            }
+        }
+
+        public void put(K key, V value) {
+            int bucketIndex = hashFunction(key);
+            int keyIndexInsideLL = searchIndexInLL(bucketIndex, key);
+            if (keyIndexInsideLL < 0) { //key does not exist in the specified bucket index
+                buckets[bucketIndex].add(new Node(key, value));
+                n++;
+            } else {
+                Node node = buckets[bucketIndex].get(keyIndexInsideLL);
+                node.value = value;
+            }
+            double lambda = (double) n / N;
+            if (lambda > THRESHOLD_OF_K) {
+                rehashing();
+            }
+        }
+
+
+        private class Node {
+            K key;
+            V value;
+
+            public Node(K key, V value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        HashMap<String,Integer> countries = new HashMap<>();
+        countries.put("India",1);
+        countries.put("Australia",2);
+        countries.put("England",3);
+
+    }
+}
--- src/stream/StreamPlaylist.java
+++ src/stream/StreamPlaylist.java
@@ -1,10 +1,6 @@
 package stream;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -106,5 +102,59 @@ public class StreamPlaylist {
         int[] elem = {1, 6, 7, 8, 1, 1, 8, 8, 7};
         System.out.println(Arrays.stream(elem).distinct().sum());
 
+
+        //find the non repeated character from a string
+        String rep = "Hello world";
+        System.out.println(rep.toLowerCase().replaceAll("[^a-z]", "").chars().mapToObj(ch -> (char) ch).collect(
+                Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                )
+        ).entrySet().stream().min(Comparator.comparing(Map.Entry::getValue)).get().getKey());
+
+
+        //find the first non-repeated character from every word of a string
+        String ques = "Hi My name is Niladri Sesen";
+        List<String> stream = Arrays.stream(ques.toLowerCase().replaceAll("[^a-z\\s]", "").split(" ")).toList();
+        System.out.println(stream.stream() // [hi, my, name, is, niladri, seen]
+                .map(ele -> Arrays.stream(ele.split("")).collect(
+                        Collectors.groupingBy(
+                                Function.identity(),
+                                LinkedHashMap::new,
+                                Collectors.counting()
+                        )
+                ).entrySet().stream().min(Comparator.comparing(element -> element.getValue())).get().getKey()).toList());
+
+
+        //find the first repeated chRcter from a sentence
+        String first = "Hello World";
+        System.out.println(Arrays.stream(first.toLowerCase().replaceAll("[^a-z]", "").split("")).collect(
+                Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                )
+        ).entrySet().stream().max(Comparator.comparing(element -> element.getValue())).get().getKey());
+
+
+        //group the numbers by the range in which they belongs to
+        int[] ao = {2, 3, 10, 14, 20, 24};
+        System.out.println(Arrays.stream(ao).boxed().collect(
+                Collectors.groupingBy(ele -> (ele.intValue() / 10) * 10)
+        ));
+
+
+        //TODO - Doubt
+        //from a given strings find out list of integers
+        //i/p-["abc","123"]
+        //o/p-[123]
+        String[] s = {"abc", "123", "def", "456"};
+        System.out.println(Arrays.stream(s).filter(e -> e.matches("[0-9]+")).map(Integer::valueOf).toList());
+
+
+        //find product of first two elements of the array
+        List<Integer> list = Arrays.asList(12, 5, 4, 5, 6);
+        System.out.println(list.stream().limit(2).reduce(1, (ax, b) -> ax * b));
     }
 }
