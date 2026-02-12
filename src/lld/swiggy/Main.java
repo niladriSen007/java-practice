5c39f5d473ee1ca6706d9964628e92cb56b9e9cc
Niladri <niladrisengit12@gmail.com>
2026-02-12 18:31:33 +0530

    Refactor Main class to enhance user cart functionality and improve restaurant search output
    - Added MenuItem import and integrated menu item addition to the user's cart.
    - Updated restaurant search output format for better clarity.
    - Removed commented-out code for cleaner implementation.
    - Ensured user cart is associated with the selected restaurant before creating an order.

--- src/lld/swiggy/Main.java
+++ src/lld/swiggy/Main.java
@@ -2,6 +2,7 @@ package lld.swiggy;
 
 import java.util.List;
 
+import lld.swiggy.models.MenuItem;
 import lld.swiggy.models.Order;
 import lld.swiggy.models.Restaurant;
 import lld.swiggy.models.User;
@@ -12,18 +13,19 @@ public class Main {
     public static void main(String[] args) {
         Swiggy swiggy = new Swiggy();
         User user = new User(1, "John Doe", "123 Main St, Anytown, USA");
-        swiggy.addToCart(user, "1");
-        swiggy.addToCart(user, "2");
-        swiggy.addToCart(user, "3");
-        swiggy.printUserCart(user);
 
         List<Restaurant> searchRestaurantsByLocation = swiggy.searchRestaurantsByLocation("Pune");
         for (Restaurant restaurant : searchRestaurantsByLocation) {
-            System.out.println(restaurant.getRestaurantName() + " - " + restaurant.getRestaurantAddress());
+            System.out.println("Search results for restaurants in " + restaurant.getRestaurantAddress() + " is -  "
+                    + restaurant.getRestaurantName());
         }
 
+        user.getCart().setRestaurant(searchRestaurantsByLocation.get(0));
+        user.getCart().addMenuItem(new MenuItem("1", "Item 1", 100));
+        user.getCart().addMenuItem(new MenuItem("2", "Item 2", 200));
+        user.getCart().addMenuItem(new MenuItem("3", "Item 3", 300));
+
         Order order = swiggy.createInstantOrder(user, "Delivery", new PaymentContext(new CreditCardPaymentStrategy()));
-        swiggy.payForOrder(user, order);
         swiggy.printUserCart(user);
 
         // Order scheduledOrder = swiggy.createScheduledOrder(user, "Delivery",
